7d828de3697156c1c130a9ffcb155e61508d0198
Bryan <85651446+Bryan-014@users.noreply.github.com>
2025-12-08 21:53:57 -0300

    Refactor login logic to use prepared statements

--- app/assets/php/login/_login.php
+++ app/assets/php/login/_login.php
@@ -1,30 +1,79 @@
 <?php
-  require_once("../util.php");
+require_once("../util.php");
 
-  if (!isset($_SESSION["user"]) || (isset($_SESSION["user"]["cpf"]) && $_SESSION["user"]["cpf"] != md5(replace_cpf($_POST["cpf"])))) {
-    $query = "select * from tbl_user where cpf = '".md5(replace_cpf($_POST["cpf"]))."'";
-    $user = mysqli_query($connection, $query);
-    $_SESSION["user"] = mysqli_fetch_assoc($user);
+if (!isset($_POST["cpf"]) || !isset($_POST["pass"])) {
+  header("Location:../../../?page=login");
+  exit();
+}
+
+$cpf_input_hash = md5(replace_cpf($_POST["cpf"]));
+$query = "SELECT * FROM tbl_user WHERE cpf = ?";
+$stmt = mysqli_prepare($connection, $query);
+mysqli_stmt_bind_param($stmt, "s", $cpf_input_hash);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+$user = mysqli_fetch_assoc($result);
+mysqli_stmt_close($stmt);
+
+$_SESSION['cpfDesc'] = $_POST["cpf"];
+
+if ($user) {
+  if (!$user['ativo']) {
+    $_SESSION["warning"] = generateWarning("Usuário inativado!");
+    unset($_SESSION["user"]);
+    header("Location:../../../?page=login");
+    mysqli_close($connection);
+    exit();
   }
-  $_SESSION['cpfDesc'] = $_POST["cpf"];
 
-  if (isset($_SESSION["user"]["usuario"])) {
-    if ($_SESSION["user"]["ativo"]) {
-      if ($_SESSION["user"]["senha"] == md5($_POST["pass"])) {
-        $_SESSION["e055fc1b8b6647e27bc36aa81b5fd7c0"] = true;
-        header("Location:../../../?page=sell");
-        exit();
-      } else {
-        $_SESSION["warning"] = generateWarning("Usuário ou senha incorretos!");
-      }
-    } else {
-      $_SESSION["warning"] = generateWarning("Usuário inativado!");
-      unset($_SESSION["user"]);
+  $stored = $user['senha'];
+
+  $login_ok = false;
+
+  if (password_verify($_POST['pass'], $stored)) {
+    $login_ok = true;
+
+    $options = ['cost' => 12];
+    if (password_needs_rehash($stored, PASSWORD_DEFAULT, $options)) {
+      $newHash = password_hash($_POST['pass'], PASSWORD_DEFAULT, $options);
+      $sql = "UPDATE tbl_user SET senha = ? WHERE id = ?";
+      $upd = mysqli_prepare($connection, $sql);
+      mysqli_stmt_bind_param($upd, "ss", $newHash, $user['id']);
+      mysqli_stmt_execute($upd);
+      mysqli_stmt_close($upd);
     }
+
+  } else {
+    if (preg_match('/^[a-f0-9]{32}$/i', $stored)) {
+      if ($stored === md5($_POST['pass'])) {
+        $login_ok = true;
+        $options = ['cost' => 12];
+        $newHash = password_hash($_POST['pass'], PASSWORD_DEFAULT, $options);
+        $sql = "UPDATE tbl_user SET senha = ? WHERE id = ?";
+        $upd = mysqli_prepare($connection, $sql);
+        mysqli_stmt_bind_param($upd, "ss", $newHash, $user['id']);
+        mysqli_stmt_execute($upd);
+        mysqli_stmt_close($upd);
+      }
+    }
+  }
+
+  if ($login_ok) {
+    $_SESSION["user"] = $user;
+    $_SESSION["e055fc1b8b6647e27bc36aa81b5fd7c0"] = true;
+    header("Location:../../../?page=sell");
+    mysqli_close($connection);
+    exit();
   } else {
     $_SESSION["warning"] = generateWarning("Usuário ou senha incorretos!");
     unset($_SESSION["user"]);
   }
-  header("Location:../../../?page=login");
-  mysqli_close($connection);
+
+} else {
+  $_SESSION["warning"] = generateWarning("Usuário ou senha incorretos!");
+  unset($_SESSION["user"]);
+}
+
+header("Location:../../../?page=login");
+mysqli_close($connection);
 ?>
