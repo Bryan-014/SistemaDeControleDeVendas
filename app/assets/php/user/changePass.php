f5774784ee981c82e9b6c6b0f88c546b8b98f6ff
Bryan <85651446+Bryan-014@users.noreply.github.com>
2025-12-08 21:55:02 -0300

    Refactor password change logic to use prepared statements and bcrypt

--- app/assets/php/user/changePass.php
+++ app/assets/php/user/changePass.php
@@ -7,12 +7,21 @@
 
   if (strlen($_POST['newPass']) < 6) {
     $_SESSION["warning"] = generateWarning("A senha deve ter mais de 6 caracteres!");
-  }elseif($_POST['newPass'] != $_POST['confirmPass']) {
+  } elseif ($_POST['newPass'] != $_POST['confirmPass']) {
     $_SESSION["warning"] = generateWarning("As senhas não são iguais!");
-  }elseif(mysqli_query($connection, "update tbl_user set senha = '".md5($_POST['newPass'])."' where id = '".$_POST['id']."'")) {
-    $_SESSION["warning"] = generateWarning("Senha alterada com sucesso!", false);
-  }else{
-    $_SESSION["warning"] = generateWarning("Senha não alterada!");
+  } else {
+    $options = ['cost' => 12];
+    $newHash = password_hash($_POST['newPass'], PASSWORD_DEFAULT, $options);
+
+    $sql = "UPDATE tbl_user SET senha = ? WHERE id = ?";
+    $stmt = mysqli_prepare($connection, $sql);
+    mysqli_stmt_bind_param($stmt, "ss", $newHash, $_POST['id']);
+    if (mysqli_stmt_execute($stmt)) {
+      $_SESSION["warning"] = generateWarning("Senha alterada com sucesso!", false);
+    } else {
+      $_SESSION["warning"] = generateWarning("Senha não alterada!");
+    }
+    mysqli_stmt_close($stmt);
   }
   header('location:../../../?page=editUser&id='.$_POST['id']);
   mysqli_close($connection);
